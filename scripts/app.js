3f7f8ddf3ea189f20e3e9fa0bb93458d5515d0be
brittonashford <brittonashford@users.noreply.github.com>
2021-12-30 23:10:20 -0600

    add modal functionality fix promise/syncronicity issues

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,8 @@
-const randomUserUrl = 'https://randomuser.me/api/?results=12'
-const userGallery = document.getElementById('gallery');
+const randomUserUrl = 'https://randomuser.me/api/?results=12&nat=us'
+const employeeGallery = document.getElementById('gallery');
+let employeeArr;
+const modal = document.getElementById('modal');
+const body = document.querySelector('body');
 
 function checkResponse(response) {
     if (response.ok) {
@@ -10,6 +13,8 @@ function checkResponse(response) {
 }
 
 function generateCardHTML(JsonObjArr) {
+    employeeArr = JsonObjArr;
+    console.log(employeeArr);
 
     for (i = 0; i < JsonObjArr.length; i++) {
         let cardHtml = `
@@ -25,46 +30,58 @@ function generateCardHTML(JsonObjArr) {
             </div>
         `;
 
-        userGallery.insertAdjacentHTML('beforeend', cardHtml);
+        employeeGallery.insertAdjacentHTML('beforeend', cardHtml);
     }
 }
 
-//function generateModalHTML(userData) {
-//    let modalHTML = `
-//    <div class="modal-container">
-//        <div class="modal">
-//            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-//            <div class="modal-info-container">
-//                <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-//                    <h3 id="name" class="modal-name cap">name</h3>
-//                    <p class="modal-text">email</p>
-//                    <p class="modal-text cap">city</p>
-//                    <hr>
-//                        <p class="modal-text">(555) 555-5555</p>
-//                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-//                        <p class="modal-text">Birthday: 10/21/2015</p>
-//                    </div>
-//                </div>
-//                <div class="modal-btn-container">
-//                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-//                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
-//                </div>
-//            </div>
-//    `;
-//}
+
+function addListeners() {
+    const cards = document.querySelectorAll('.card'); 
+
+    cards.forEach(function(card, index){
+        card.addEventListener('click', e => {
+            generateModalHTML(index);
+        })
+    })
+}
+
+function generateModalHTML(index) {
+    console.log(index);
+    console.log(employeeArr[index]);
+
+    const formattedBday = new Date(employeeArr[index].dob.date).toLocaleDateString;
+
+    let modalHTML = `
+    <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src=${employeeArr[index].picture.large} alt="profile picture">
+                <h3 id="name" class="modal-name cap">${employeeArr[index].name.first} ${employeeArr[index].name.last}</h3>
+                <p class="modal-text">${employeeArr[index].email}</p>
+                <p class="modal-text cap">${employeeArr[index].location.city}</p>
+                <hr>
+                <p class="modal-text">${employeeArr[index].phone}</p>
+                <p class="modal-text">${employeeArr[index].location.street.number} ${employeeArr[index].location.street.name} ${employeeArr[index].location.city}, ${employeeArr[index].location.state} ${employeeArr[index].location.postcode}</p>
+                <p class="modal-text">Birthday: ${formattedBday}</p>
+            </div>
+        <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>
+    </div>   
+    </div>
+    `;
+
+    body.insertAdjacentHTML('beforeend', modalHTML);
+}
 
 
 fetch(randomUserUrl)
     .then(response => checkResponse(response))
     .then(response => response.json())
     .then(data => generateCardHTML(data.results))
+    .then(cards => addListeners())
     .catch(error => console.log('An error has occurred.', error))
 
-const cards = document.querySelectorAll('.card');  //card elements don't exist yet when this is called?
-console.log(cards);
 
-cards.forEach(card => {
-    card.addEventListener('click', e => {
-        console.log(e.target);
-    })
-})
