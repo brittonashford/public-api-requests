4d1de35c38d52abfbcec77714a75570f43082b66
brittonashford <brittonashford@users.noreply.github.com>
2021-12-30 20:23:49 -0600

    troubleshooting async/await/promises

--- scripts/app.js
+++ scripts/app.js
@@ -9,7 +9,7 @@ function checkResponse(response) {
     }
 }
 
-function generateHTML(JsonObjArr) {
+function generateCardHTML(JsonObjArr) {
 
     for (i = 0; i < JsonObjArr.length; i++) {
         let cardHtml = `
@@ -29,8 +29,42 @@ function generateHTML(JsonObjArr) {
     }
 }
 
+//function generateModalHTML(userData) {
+//    let modalHTML = `
+//    <div class="modal-container">
+//        <div class="modal">
+//            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+//            <div class="modal-info-container">
+//                <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+//                    <h3 id="name" class="modal-name cap">name</h3>
+//                    <p class="modal-text">email</p>
+//                    <p class="modal-text cap">city</p>
+//                    <hr>
+//                        <p class="modal-text">(555) 555-5555</p>
+//                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+//                        <p class="modal-text">Birthday: 10/21/2015</p>
+//                    </div>
+//                </div>
+//                <div class="modal-btn-container">
+//                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+//                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+//                </div>
+//            </div>
+//    `;
+//}
+
+
 fetch(randomUserUrl)
     .then(response => checkResponse(response))
     .then(response => response.json())
-    .then(data => generateHTML(data.results))
+    .then(data => generateCardHTML(data.results))
     .catch(error => console.log('An error has occurred.', error))
+
+const cards = document.querySelectorAll('.card');  //card elements don't exist yet when this is called?
+console.log(cards);
+
+cards.forEach(card => {
+    card.addEventListener('click', e => {
+        console.log(e.target);
+    })
+})
