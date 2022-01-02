2b61ba2f076dd48fea6db7594aa29a90962ade55
brittonashford <brittonashford@users.noreply.github.com>
2022-01-02 16:08:57 -0600

    add comments and clean up code

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,11 @@
+//global variables
 const randomUserUrl = 'https://randomuser.me/api/?results=12&nat=us'
 const employeeGallery = document.getElementById('gallery');
-let employeeArr;
-const modal = document.getElementById('modal');
 const body = document.querySelector('body');
+let employeeArr;
+let modalContainer;
 
+//checks the status of the fetch API request and handles accordingly
 function checkResponse(response) {
     if (response.ok) {
         return Promise.resolve(response);
@@ -12,9 +14,9 @@ function checkResponse(response) {
     }
 }
 
+//loops through the random "employees" retrieved from the API and creates cards for them
 function generateCardHTML(JsonObjArr) {
     employeeArr = JsonObjArr;
-    console.log(employeeArr);
 
     for (i = 0; i < JsonObjArr.length; i++) {
         let cardHtml = `
@@ -30,12 +32,12 @@ function generateCardHTML(JsonObjArr) {
             </div>
         `;
 
-        employeeGallery.insertAdjacentHTML('beforeend', cardHtml);
+        employeeGallery.insertAdjacentHTML('beforeend', cardHtml);       
     }
 }
 
-
-function addListeners() {
+//creates event listener for each of the user cards that will dynamically generate a modal view of the given user
+function addCardListeners() {
     const cards = document.querySelectorAll('.card'); 
 
     cards.forEach(function(card, index){
@@ -45,16 +47,15 @@ function addListeners() {
     })
 }
 
+//generates modal content/view, appends it to the body, and calls method to generate close functionality
 function generateModalHTML(index) {
-    console.log(index);
-    console.log(employeeArr[index]);
-
-    const formattedBday = new Date(employeeArr[index].dob.date).toLocaleDateString;
+    const bday = new Date(employeeArr[index].dob.date);
+    const formattedBday = ((bday.getMonth() + 1) + '/' + bday.getDate()) + '/' + bday.getFullYear();
 
     let modalHTML = `
     <div class="modal-container">
         <div class="modal">
-            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <button type="button" id="modal-close-btn" class="modal-close-btn btn"><strong>X</strong></button>
             <div class="modal-info-container">
                 <img class="modal-img" src=${employeeArr[index].picture.large} alt="profile picture">
                 <h3 id="name" class="modal-name cap">${employeeArr[index].name.first} ${employeeArr[index].name.last}</h3>
@@ -65,23 +66,37 @@ function generateModalHTML(index) {
                 <p class="modal-text">${employeeArr[index].location.street.number} ${employeeArr[index].location.street.name} ${employeeArr[index].location.city}, ${employeeArr[index].location.state} ${employeeArr[index].location.postcode}</p>
                 <p class="modal-text">Birthday: ${formattedBday}</p>
             </div>
-        <div class="modal-btn-container">
-            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-            <button type="button" id="modal-next" class="modal-next btn">Next</button>
-        </div>
-    </div>   
+        </div>   
     </div>
     `;
 
     body.insertAdjacentHTML('beforeend', modalHTML);
+    modalContainer = document.querySelector('.modal-container');
+    addCloseListeners();
 }
 
+//these listeners close the modal view when the user click the "X" button or clicks away from the modal
+function addCloseListeners() {
+    const closeBtn = document.getElementById('modal-close-btn');
+    const prevBtn = document.getElementById('modal-prev');
+    const nextBtn = document.getElementById('modal-next');
 
+    closeBtn.addEventListener('click', e => {
+        modalContainer.remove();
+    })
+
+    modalContainer.addEventListener('click', e => {
+        let target = e.target;
+        if (e.target === modalContainer) {
+            modalContainer.remove();
+        }
+    });
+}
+
+//fetch API used to retrieve random fictitious "employee" data from randomuser.me API
 fetch(randomUserUrl)
     .then(response => checkResponse(response))
     .then(response => response.json())
     .then(data => generateCardHTML(data.results))
-    .then(cards => addListeners())
+    .then(cards => addCardListeners())
     .catch(error => console.log('An error has occurred.', error))
-
-
